a23cdebdc8616b7f0a1f1878c1e40bcd51877256
Simas Toleikis <simas.toleikis@adform.com>
2018-02-09 15:44:51 +0200

    Split node_modules into separate vendors chunk.

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,10 +8,12 @@ const outputDir = path.resolve(__dirname, STATIC_DIR)
 
 // Webpack build configuration for client-side code distribution
 const config: webpack.Configuration = {
-	entry: './src/client/index.tsx',
+	entry: {
+		app: './src/client/index.tsx'
+	},
 	output: {
 		path: outputDir,
-		filename: 'bundle.js'
+		filename: '[name].js',
 	},
 	devtool: 'source-map',
 	resolve: {
@@ -36,17 +38,33 @@ const config: webpack.Configuration = {
 			}
 		]
 	},
-	plugins: [
-		new webpack.optimize.ModuleConcatenationPlugin(),
-		new HtmlWebpackPlugin({
-			title: 'Wurm Online Maps',
-			filename: INDEX_FILE,
-			hash: true,
-			minify: {
-				collapseWhitespace: true
-			}
-		})
-	]
+	plugins: getPlugins()
+}
+
+function getPlugins(): webpack.Plugin[] {
+
+	const plugins = []
+
+	// Enable module scope hoisting
+	plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
+
+	// Generate main HTML file
+	plugins.push(new HtmlWebpackPlugin({
+		title: 'Wurm Online Maps',
+		filename: INDEX_FILE,
+		hash: true,
+		minify: {
+			collapseWhitespace: true
+		}
+	}))
+
+	// Split all dependencies from node_modules into separate vendors.js chunk
+	plugins.push(new webpack.optimize.CommonsChunkPlugin({
+		name: 'vendors',
+		minChunks: (module) => module.context && module.context.indexOf('node_modules') !== -1
+	}))
+
+	return plugins
 }
 
 export default config
